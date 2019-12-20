db54e4e298007bd1c83d3fa98bdf6e8563606905
muhammadjewel <msuratov@gmail.com>
2019-12-20 23:15:06 +0500

    Write SELECT queries

--- queries.sql
+++ queries.sql
@@ -19,3 +19,27 @@ INSERT INTO posts (title, body, author, image, video, link, views, user_id, cont
 /* Create comments for posts */
 INSERT INTO comments (body, user_id, post_id) VALUES ('Классно', 1, 1);
 INSERT INTO comments (body, user_id, post_id) VALUES ('Очень жду.', 2, 2);
+
+/* получить список постов с сортировкой по популярности и вместе с именами авторов и типом контента */
+SELECT posts.*, users.login, content_types.type_name FROM posts
+JOIN users
+  ON posts.user_id = users.id
+JOIN content_types
+  ON posts.content_type_id = content_types.id
+ORDER BY views DESC;
+
+/* получить список постов для конкретного пользователя */
+SELECT * FROM posts
+WHERE posts.user_id = 1;
+
+/* получить список комментариев для одного поста, в комментариях должен быть логин пользователя */
+SELECT comments.*, users.login FROM comments
+JOIN users
+  ON comments.user_id = users.id
+WHERE comments.post_id = 1;
+
+/* добавить лайк к посту */
+INSERT INTO likes(user_id, post_id) VALUES (1, 2);
+
+/* подписаться на пользователя */
+INSERT INTO subscriptions(author_id, subscription) VALUES (1, 2);
