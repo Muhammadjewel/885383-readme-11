d1dda71a107d2f9815d56e5d9e8d66dc9f8b46b9
muhammadjewel <msuratov@gmail.com>
2019-12-22 14:50:57 +0500

    Add href value to posts with link type

--- templates/main.php
+++ templates/main.php
@@ -112,7 +112,7 @@ if (!$connection) {
                 </blockquote>
                 <?php elseif ($post['class'] == 'link'): ?>
                 <div class="post-link__wrapper">
-                    <a class="post-link__external" href="http://" title="Перейти по ссылке">
+                    <a class="post-link__external" href="https://<?=$post['link']; ?>" title="Перейти по ссылке">
                         <div class="post-link__info-wrapper">
                             <div class="post-link__icon-wrapper">
                                 <img src="https://www.google.com/s2/favicons?domain=vitadental.ru" alt="Иконка">
