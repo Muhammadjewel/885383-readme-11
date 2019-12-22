a81df6ad545a51bd0f8bc473d4ae341be9ab4690
muhammadjewel <msuratov@gmail.com>
2019-12-22 20:45:08 +0500

    Get posts based on content type filters

--- popular.php
+++ popular.php
@@ -1,15 +1,23 @@
 <?php
 require_once('helpers.php');
 
+$selectContentTypesSql = 'SELECT * FROM content_types';
+$contentTypes = dbFetchData($connection, $selectContentTypesSql);
+
 $contentTypeQuery = filter_input(INPUT_GET, 'content_type', FILTER_SANITIZE_NUMBER_INT);
 
-$selectContentTypesQuery = 'SELECT * FROM content_types ORDER BY class ASC';
-
-$contentTypes = dbFetchData($connection, $selectContentTypesQuery);
+if ($contentTypeQuery) {
+    $selectPostsSql = 'SELECT * FROM posts WHERE content_type_id = ? ORDER BY views DESC';
+    $posts = dbFetchData($connection, $selectPostsSql, [$contentTypeQuery]);
+} else {
+    $selectPostsSql = 'SELECT * FROM posts ORDER BY views DESC';
+    $posts = dbFetchData($connection, $selectPostsSql);
+}
 
 $pageContent = include_template('popular.php', [
     'contentTypeQuery' => $contentTypeQuery,
     'contentTypes' => $contentTypes,
+    'posts' => $posts
 ]);
 
 $layoutContent = include_template('layout.php', [
