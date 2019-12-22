00606dfc9cb569a2b0d96cda3c1539fbd48334c3
muhammadjewel <msuratov@gmail.com>
2019-12-22 21:26:44 +0500

    Get post data from DB

--- post.php
+++ post.php
@@ -1,7 +1,18 @@
 <?php
 require_once('helpers.php');
 
-$pageContent = include_template('post.php');
+$postIdQuery = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+
+if ($postIdQuery) {
+    $selectPostByIdSql = 'SELECT * FROM posts JOIN users ON posts.user_id = users.id WHERE posts.id = ?';
+    $post = dbFetchData($connection, $selectPostByIdSql, [$postIdQuery])[0];
+} else {
+    // 404
+}
+
+$pageContent = include_template('post.php', [
+    'post' => $post
+]);
 
 $layoutContent = include_template('layout.php', [
     'pageContent' => $pageContent,
