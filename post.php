e7bc51b5d4f47264cbc00c82c5efd370a8f12906
muhammadjewel <msuratov@gmail.com>
2019-12-23 19:03:41 +0500

    Show fitting post type template and post's author login and avatar

--- post.php
+++ post.php
@@ -4,14 +4,27 @@ require_once('helpers.php');
 $postIdQuery = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 
 if ($postIdQuery) {
-    $selectPostByIdSql = 'SELECT * FROM posts JOIN users ON posts.user_id = users.id WHERE posts.id = ?';
+    $selectPostByIdSql = 'SELECT posts.*, content_types.class AS class, users.login AS author, users.avatar AS author_avatar FROM posts JOIN users ON posts.user_id = users.id JOIN content_types ON posts.content_type_id = content_types.id WHERE posts.id = ?';
     $post = dbFetchData($connection, $selectPostByIdSql, [$postIdQuery])[0];
+
+    if ($post['class'] == 'quote') {
+        $postBody = include_template('post-quote.php', ['post' => $post]);
+    } elseif ($post['class'] == 'text') {
+        $postBody = include_template('post-text.php', ['post' => $post]);
+    } elseif ($post['class'] == 'link') {
+        $postBody = include_template('post-link.php', ['post' => $post]);
+    } elseif ($post['class'] == 'photo') {
+        $postBody = include_template('post-photo.php', ['post' => $post]);
+    } elseif ($post['class'] == 'video') {
+        $postBody = include_template('post-video.php', ['post' => $post]);
+    }
 } else {
     // 404
 }
 
 $pageContent = include_template('post.php', [
-    'post' => $post
+    'post' => $post,
+    'postBody' => $postBody
 ]);
 
 $layoutContent = include_template('layout.php', [
